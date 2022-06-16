cee3c826010f72a6f73826d46885bbb30d8c5a8c
Anshjain123 <anshjainalp@gmail.com>
2022-06-17 00:42:10 +0530

    location added

--- App.js
+++ App.js
@@ -1,10 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaAndroid } from './Components/AndroidSafeView';
+import { useState, useEffect } from 'react'
+import * as Location from 'expo-location';
+
 export default function App() {
+
+
+
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+
+  useEffect(() => {
+    async function getlocation() {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+      let location = await Location.getCurrentPositionAsync({
+        accuracy:Location.Accuracy.Highest
+      });
+      setLocation(location);
+      console.log(location); 
+    }
+    getlocation();
+  }, [])
+  let text = "Watign";
+  // if (errorMsg) {
+    // text = errorMsg;
+  // } else if (location) {
+    // text = location
+  
   return (
-    <View styles = {SafeAreaAndroid.SafeArea}>
-      <Text>Front End Part</Text>
-      <StatusBar style="auto" />
+    <View style={SafeAreaAndroid.SafeArea}>
+      {/* <Text>{text}</Text> */}
+      {/* <StatusBar style="auto" /> */}
+      <Text>{text}</Text>
     </View>
   );
 }
@@ -14,6 +47,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
 });
