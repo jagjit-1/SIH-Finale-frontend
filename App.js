fec37dd4cf6cb0c365e14b25db6fe45d61d7050e
Anshjain123 <anshjainalp@gmail.com>
2022-06-18 02:03:22 +0530

    Navigation added

--- App.js
+++ App.js
@@ -1,14 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaAndroid } from './Components/AndroidSafeView';
+import HomeScreen from './Components/Screens/HomeScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+import AboutScreen from './Components/Screens/AboutScreen';
+import ProfileScreen from './Components/Screens/ProfileScreen';
+import { AntDesign } from '@expo/vector-icons';
+
 
 export default function App() {
-
-  let text = "Watign"; 
+  const Stack = createNativeStackNavigator();
+  const Tab = createBottomTabNavigator();
+  let text = "Watign";
   return (
-    <View style={SafeAreaAndroid.SafeArea}>
-      <Text>{text}</Text>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator initialRouteName='Home' screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          if (route.name === "Home") {
+ 
+            return <Ionicons name="ios-home" size={size} color={color} />;
+          } else if (route.name === "About") {
+            return <FontAwesome name="question-circle-o" size={size} color={color} />
+          } else if (route.name === "Profile") {
+            return <AntDesign name="profile" size={size} color={color} />
+          }
+        },
+        tabBarActiveTintColor: '#FF9933',
+        tabBarInactiveTintColor: 'gray',
+      })}>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="About" component={AboutScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
