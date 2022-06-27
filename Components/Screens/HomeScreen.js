424986b1e82ee7a133957635cbcf3bb4f63478ac
Anshjain123 <anshjainalp@gmail.com>
2022-06-28 01:10:48 +0530

    linear gradient added

--- Components/Screens/HomeScreen.js
+++ Components/Screens/HomeScreen.js
@@ -1,30 +1,38 @@
 import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 import { FontAwesome } from '@expo/vector-icons';
+import { LinearGradient } from 'expo-linear-gradient';
+
 
 const HomeScreen = () => {
   return (
-    <View style={styles.homescreen} >
-      <View>
-        <TouchableOpacity style={styles.homescreen__button} activeOpacity={0.4}>
-          <Image style={styles.homescreen__image} source={require("../images/mark_attendance.jpg")} />
-          <Text style={styles.homescreen__text}>
-            Mark Your Attendance
-          </Text>
-          <FontAwesome name="arrow-circle-right" size={24} color="black" />
-        </TouchableOpacity>
-      </View>
+    <LinearGradient
+      colors={['rgba(255,173,3,1)', 'rgba(235,243,220,1)', 'rgba(146,182,41,1)', 'rgba(145,181,39,1)']}
+      end={{x:0.5, y:1.4}} 
+    >
+      <View style={styles.homescreen} >
+        <View>
+          <TouchableOpacity style={styles.homescreen__button} activeOpacity={0.4}>
+            <Image style={styles.homescreen__image} source={require("../images/mark_attendance.jpg")} />
+            <Text style={styles.homescreen__text}>
+              Mark Your Attendance
+            </Text>
+            <FontAwesome name="arrow-circle-right" size={24} color="black" />
+          </TouchableOpacity>
+        </View>
 
-      <View>
-        <TouchableOpacity style={styles.homescreen__button} activeOpacity={0.4}>
-          <Image style={styles.homescreen__image} source={require("../images/register_now.jpg")} />
-          <Text style={styles.homescreen__text}>
-            New Registration
-          </Text>
-          <FontAwesome name="arrow-circle-right" size={24} color="black" />
-        </TouchableOpacity>
+        <View>
+          <TouchableOpacity style={styles.homescreen__button} activeOpacity={0.4}>
+            <Image style={styles.homescreen__image} source={require("../images/register_now.jpg")} />
+            <Text style={styles.homescreen__text}>
+              New Registration
+            </Text>
+            <FontAwesome name="arrow-circle-right" size={24} color="black" />
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </LinearGradient>
+
   )
 }
 export default HomeScreen
@@ -35,6 +43,8 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-around',
     marginTop: 100,
+    height:'100%'
+    
   },
   homescreen__button: {
     elevation: 10,
