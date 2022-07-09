9d43c4cee26d25ddebd0b7df2be319075b3c4dac
Anshjain123 <anshjainalp@gmail.com>
2022-07-09 22:29:53 +0530

    Calendar added

--- Components/Screens/AttendanceScreen.js
+++ Components/Screens/AttendanceScreen.js
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View, ImageBackground } from 'react-native'
 import React, { useLayoutEffect } from 'react'
-import { ScaledSheet, } from 'react-native-size-matters';
+import { ScaledSheet, moderateScale} from 'react-native-size-matters';
 import { Avatar, Image } from '@rneui/base';
 import { Calendar } from 'react-native-calendars';
 import { Octicons } from '@expo/vector-icons';
@@ -17,50 +17,54 @@ const AttendanceScreen = ({ navigation }) => {
         <View>
           <Avatar rounded source={{ uri: "https://wallpaperaccess.com/full/749931.jpg" }} containerStyle={{ marginRight: 20 }} />
         </View>
-      )
+      ),
+      headerTintColor: 'white',
+      headerStyle: {
+        backgroundColor: 'rgb(120, 117, 248)'
+      }
     })
   }, [navigation])
 
   return (
     <View style={styles.container}>
-      <ImageBackground source={require("../images/customdrawer.jpeg")} style={styles.container__image}>
-        <Text style={styles.container__text}>Your Attendance Record</Text>
-      </ImageBackground>
-      <Calendar
-      style={styles.calendar}
-        theme={{
-          backgroundColor: '#ffffff',
-          calendarBackground: '#ffffff',
-          textSectionTitleColor: '#b6c1cd',
-          textSectionTitleDisabledColor: '#d9e1e8',
-          selectedDayBackgroundColor: '#00adf5',
-          selectedDayTextColor: '#ffffff',
-          todayTextColor: '#00adf5',
-          dayTextColor: '#2d4150',
-          textDisabledColor: '#d9e1e8',
-          dotColor: '#00adf5',
-          selectedDotColor: '#ffffff',
-          arrowColor: 'black',
-          disabledArrowColor: '#d9e1e8',
-          monthTextColor: 'blue',
-          indicatorColor: 'blue',
-          textDayFontFamily: 'monospace',
-          textMonthFontFamily: 'monospace',
-          textDayHeaderFontFamily: 'monospace',
-          textDayFontWeight: '500',
-          textMonthFontWeight: 'bold',
-          textDayHeaderFontWeight: '500',
-          textDayFontSize: 16,
-          textMonthFontSize: 16,
-          textDayHeaderFontSize: 16
-        }}
-        enableSwipeMonths={true}
-        markedDates={{
-          '2022-07-06': { selected: true, marked: true, selectedColor: 'rgb(83, 189, 235)' },
-          '2022-07-01': { selected: true, marked: true, selectedColor: '#FFC107' },
-          '2022-07-07': { selected: true, marked: true, selectedColor: '#FF5722' },
-        }}
-      />
+      <View style={styles.Calendar__view}>
+        <Text style={styles.Calendar__text}>Calendar</Text>
+        <Calendar
+          style={styles.Calendar}
+          theme={{
+
+            backgroundColor: 'rgb(232, 234, 237)',
+            calendarBackground: 'rgb(232, 234, 237)',
+            textSectionTitleColor: '#b6c1cd',
+            textSectionTitleDisabledColor: '#d9e1e8',
+            selectedDayBackgroundColor: '#00adf5',
+            selectedDayTextColor: '#ffffff',
+            todayTextColor: '#00adf5',
+            dayTextColor: '#2d4150',
+            textDisabledColor: '#d9e1e8',
+            dotColor: '#00adf5',
+            selectedDotColor: '#ffffff',
+            arrowColor: 'black',
+            disabledArrowColor: '#d9e1e8',
+            monthTextColor: 'blue',
+            indicatorColor: 'blue',
+            textDayFontWeight: '500',
+            textMonthFontWeight: 'bold',
+            textDayHeaderFontWeight: '500',
+            textDayFontSize: 16,
+            textMonthFontSize: 16,
+            textDayHeaderFontSize: 16
+          }}
+
+          enableSwipeMonths={true}
+          markedDates={{
+            '2022-07-06': { selected: true, marked: true, selectedColor: 'rgb(83, 189, 235)' },
+            '2022-07-01': { selected: true, marked: true, selectedColor: '#FFC107' },
+            '2022-07-07': { selected: true, marked: true, selectedColor: '#FF5722' },
+          }}
+
+        />
+      </View>
       <View style={styles.info}>
         <View style={styles.info}>
           <Octicons name="dot-fill" size={24} color="rgb(83, 189, 235)" style={{ marginRight: 5 }} />
@@ -76,8 +80,6 @@ const AttendanceScreen = ({ navigation }) => {
           <Octicons name="dot-fill" size={24} color="#FFC107" style={{ marginRight: 5 }} />
           <Text>Holiday</Text>
         </View>
-
-
       </View>
     </View>
   )
@@ -86,18 +88,17 @@ const AttendanceScreen = ({ navigation }) => {
 export default AttendanceScreen
 
 const styles = ScaledSheet.create({
-  container:{
-    backgroundColor:'white',
-    height:'100%'
-  },
-  container__image: {
-    width: '100%',
-    height: '180@ms',
+  container: {
+    backgroundColor: 'white',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    flex: 1
   },
   container__text: {
     fontSize: '25@ms',
     textAlign: 'center',
-    height: "160@ms",
+    height: "auto",
     textAlignVertical: 'center',
     color: 'white',
   },
@@ -105,7 +106,19 @@ const styles = ScaledSheet.create({
     display: 'flex',
     flexDirection: 'row',
     margin: '6@ms',
-    alignSelf: 'flex-end',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
   },
+  Calendar: {
+    height: "380@ms",
+    margin: "20@ms",
+    elevation: 25,
+    borderRadius: '20@ms',
+  },
+  Calendar__text:{
+    display:'flex',
+    flexDirection:'row',
+    fontSize:"25@ms",
+    marginLeft:"25@ms",
+    marginTop:"10@ms"
+  }
 })
