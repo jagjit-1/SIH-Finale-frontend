d6e4f45b0406189b58d802e10e148aba394f78f2
Anshjain123 <anshjainalp@gmail.com>
2022-08-23 16:18:13 +0530

    firebase

--- Components/Screens/LoginScreen.js
+++ Components/Screens/LoginScreen.js
@@ -1,9 +1,11 @@
 import { StyleSheet, Text, View, KeyboardAvoidingView, ScrollView, TouchableNativeFeedback, Dimensions } from 'react-native'
-import React, { useContext, useLayoutEffect } from 'react'
+import React, { useContext, useLayoutEffect, useState } from 'react'
 import { Image, Input } from "@rneui/themed";
 import { Button } from "@rneui/base";
 import { useHeaderHeight } from '@react-navigation/elements';
 import { AuthContext } from '../../AuthProvider';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { db, auth } from "../../Firebase/firebase"
 
 const LoginScreen = ({ navigation }) => {
   const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext)
@@ -19,38 +21,61 @@ const LoginScreen = ({ navigation }) => {
   const headerHeight = useHeaderHeight();
 
   const { width, height } = Dimensions.get('window');
+
+
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+
+
+  const HandleLogin = () => {
+
+    setIsLoggedIn(!isLoggedIn);    
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log(userCredential);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      })
+  }
+
   return (
     <KeyboardAvoidingView style={{ padding: 20, width: '100%', height: height - headerHeight, display: 'flex', flexDirection: 'column', justifyContent: 'center', flex: 1 }} >
       <View style={{ display: 'flex', flexDirection: "column", flex: 0.5, justifyContent: 'space-around' }} showsVerticalScrollIndicator={false}>
+        <ScrollView>
+          <View >
+            <Text style={{ fontSize: 40, color: '#3700B3' }}>Welcome back!</Text>
+          </View>
 
-        <View >
-          <Text style={{ fontSize: 40, color: '#3700B3' }}>Welcome back!</Text>
-        </View>
-
-        <View>
           <View>
-            <Input
-              placeholder='email@address.com'
-              label="Enter your mail"
-              labelStyle={{ color: 'black', marginBottom: 5 }}
-              leftIcon={{ type: 'Entypo', name: 'mail', color: 'gray', marginRight: 3 }}
-              keyboardType="email-address"
-            />
-          </View>
-          <View>
-            <Input
-              placeholder='Password'
-              label="Enter your Password"
-              labelStyle={{ color: 'black', marginBottom: 5 }}
-              leftIcon={{ type: 'Entypo', name: 'mail', color: 'gray', marginRight: 3 }}
-              keyboardType="email-address"
-            />
-          </View>
+            <View>
+              <Input
+                placeholder='email@address.com'
+                label="Enter your mail"
+                labelStyle={{ color: 'black', marginBottom: 5 }}
+                leftIcon={{ type: 'Entypo', name: 'mail', color: 'gray', marginRight: 3 }}
+                keyboardType="email-address"
+                onChangeText={(text) => setemail(text)}
+              />
+            </View>
+            <View>
+              <Input
+                placeholder='Password'
+                label="Enter your Password"
+                labelStyle={{ color: 'black', marginBottom: 5 }}
+                leftIcon={{ type: 'Entypo', name: 'mail', color: 'gray', marginRight: 3 }}
+                keyboardType="visible-password"
+                secureTextEntry={true}
+                onChangeText={(pass) => setpassword(pass)}
+              />
+            </View>
 
-          <View style={{ marginBottom: 15 }}>
-            <Button raised color="#5379f6" type="outline" title="Continue" TouchableComponent={TouchableNativeFeedback} onPress={() => setIsLoggedIn(!isLoggedIn)} />
+            <View style={{ marginBottom: 15 }}>
+              <Button raised color="#5379f6" type="outline" title="Continue" TouchableComponent={TouchableNativeFeedback} onPress={() => HandleLogin()} />
+            </View>
           </View>
-        </View>
+        </ScrollView>
+
 
 
       </View>
